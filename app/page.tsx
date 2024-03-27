ce300b14951ac267280700ddc61b70b16b9056c9
menma275 <ohsusie9@gmail.com>
2024-03-28 00:02:14 +0900

    ファビコン変更

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { CiShuffle } from "react-icons/ci";
 import { CiCircleQuestion } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 import somokuto from '../public/somokuto.json';
 
 export default function Home() {
@@ -15,7 +16,7 @@ export default function Home() {
     somokuto.map((line, index) => {
       const p = document.createElement('p');
       p.textContent = line;
-      p.className = "h-full text-center text-black text-md font-bold tracking-[7px]";
+      p.className = "h-full text-center text-white text-md  tracking-[7px]";
       p.style.writingMode = "vertical-rl";
       p.style.scrollSnapAlign = "start";
       haiku.appendChild(p);
@@ -23,21 +24,20 @@ export default function Home() {
   }
   
   return (
-    <main className="flex min-h-screen">
+    <main className="flex min-h-screen cursor-default">
       <div className='fixed w-full h-full py-0 sm:py-24 -z-50'>
         <img src={"mountain.jpg"} alt="background" className="w-full h-full object-cover sm:object-contain" />
       </div>
-      <div className="h-full fixed top-0 pt-12 pb-6 right-12 text-white sm:text-black tracking-[12px]">
+      <div className="h-full fixed top-0 pt-12 pb-6 right-6 text-white sm:text-black tracking-[12px]">
         <div className="flex flex-col h-full justify-between items-center">
-          <h1 style={{writingMode:"vertical-rl"}}>種田山頭火 草木塔</h1>
+          <h1 className="text-xs" style={{writingMode:"vertical-rl"}}>種田山頭火 草木塔</h1>
           <div className="flex flex-col items-center">
-            {/* <a href="#" target="_blank">
-              <CiCircleQuestion className=" opacity-50 text-xl  text-white sm:text-black" />
-            </a> */}
-            {/* シャッフルボタン */}
             <button onClick={shuffle} className="flex items-center justify-center w-12 h-12 text-white sm:text-black rounded-full">
               <CiShuffle className="text-xl" />
             </button>
+            <a href="https://github.com/menma275/somokuto" target="_blank">
+              <FaGithub className="text-xl"/>
+            </a>
           </div>
         </div>
       </div>
